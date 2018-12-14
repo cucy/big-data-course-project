f7609014956199296f7ac19055157389e21f23d2
Wei <wesleyoneway@hotmail.com>
2018-12-14 15:05:31 -0500

    delete useless files

--- run_whole_project/debug/run_in_spark.sh
+++
@@ -1,1 +0,0 @@
-/Users/wesley/important/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --master local[2] --jars elasticsearch-spark-20_2.11-6.5.2.jar /Users/wesley/codes/python/test/main_data_processor.py
